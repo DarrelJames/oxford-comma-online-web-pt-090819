b4a86ca331c88bdede2c5d70a5455db545571fd5
Darrel C <dcastellano2007@gmail.com>
2019-09-09 22:55:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  newarr = []
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size >= 3
+    array.each do |ele|
+      ele == array.last ? newarr << "and #{ele}" : newarr << ele
+    end
+    newarr.join(", ")
+  else
+    array[0]
+  end
 end
